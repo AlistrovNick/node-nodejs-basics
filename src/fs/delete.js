d92f906752b85b87d87d31f15edda2ff09fea5e8
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-04 17:05:51 +0400

    feat: resolve delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,22 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filename = 'fileToRemove.txt';
+
+const isExist = (path) => {
+    return fs.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
+
 export const remove = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, filename);
+    let isFileNotExist = !(await isExist(filePath));
+
+    if (isFileNotExist) {
+        throw new Error('FS operation failed');
+    }
+    fs.unlink(filePath);
 };
