df880302b55658558024d4114eb9409af4e85662
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-04 21:37:33 +0400

    feat: resolve list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,24 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const srcDir = 'files';
+
+const isExist = (path) => {
+    return fs.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
+
 export const list = async () => {
-    // Write your code here 
+    const srcDirPath = path.join(__dirname, srcDir);
+    let isDirNotExist = !(await isExist(srcDirPath));
+
+    if (isDirNotExist) {
+        throw new Error('FS operation failed');
+    }
+
+    let files = await fs.readdir(srcDirPath);
+    files.forEach(file => console.log(file));
 };
