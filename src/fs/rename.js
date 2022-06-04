761d9af75eedaa894372bc6ff0ffeb2ca9306e25
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-04 16:55:07 +0400

    feat: resolve rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,26 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const srcFilename = 'wrongFilename.txt';
+const destFilename = 'properFilename.md';
+
+const isExist = (path) => {
+    return fs.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
+
 export const rename = async () => {
-    // Write your code here 
+    const srcFilePath = path.join(__dirname, srcFilename);
+    const destFilePath = path.join(__dirname, destFilename);
+
+    let isSrcExist = await isExist(srcFilePath);
+    let isDestExist = await isExist(destFilePath);
+
+    if (!isSrcExist || isDestExist) {
+        throw new Error('FS operation failed');
+    }
+    fs.rename(srcFilePath, destFilePath);
 };
