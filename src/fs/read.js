7c9e2b0f77ea444badc029d9b235e9fd6699dcff
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-04 21:45:15 +0400

    feat: resolve read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,25 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const srcDir = 'files';
+const filename = 'fileToRead.txt';
+
+const isExist = (path) => {
+    return fs.access(path)
+        .then(() => true)
+        .catch(() => false);
+};
+
 export const read = async () => {
-    // Write your code here 
+    const filePath = path.join(__dirname, srcDir, filename);
+    let isFileNotExist = !(await isExist(filePath));
+
+    if (isFileNotExist) {
+        throw new Error('FS operation failed');
+    }
+
+    let data = await fs.readFile(filePath, { encoding: 'utf-8' });
+    console.log(data);
 };
