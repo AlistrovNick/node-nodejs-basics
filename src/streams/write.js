1a090914328bda39778c5175f861c2724af91850
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-05 13:51:02 +0400

    feat: resolve streams/write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const srcDir = 'files';
+const srcFile = 'fileToWrite.txt';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, srcDir, srcFile);
+
 export const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(filePath);
+    process.stdin.pipe(writeStream);
 };
