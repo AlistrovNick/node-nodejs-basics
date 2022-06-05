215de2dc9d4694f57922d5277309dab720aae949
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-05 13:46:22 +0400

    feat: resolve streams/read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const srcDir = 'files';
+const srcFile = 'fileToRead.txt';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, srcDir, srcFile);
+
 export const read = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(filePath);
+    readStream.pipe(process.stdout);
 };
