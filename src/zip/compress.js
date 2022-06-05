13d7ab364484199daf99bdc24c190cfb2b6f952b
Mikalai Alistrau <alistrovn@gmail.com>
2022-06-05 15:13:35 +0400

    feat: resolve zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,19 @@
+import zlib from 'zlib';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const srcDir = 'files';
+const srcFile = 'fileToCompress.txt';
+const archiveFile = 'archive.gz';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.join(__dirname, srcDir, srcFile);
+const archivePath = path.join(__dirname, srcDir, archiveFile);
+
 export const compress = async () => {
-    // Write your code here 
+    const readStream = fs.createReadStream(filePath);
+    const writeStream = fs.createWriteStream(archivePath);
+    const zip = zlib.createGzip();
+    readStream.pipe(zip).pipe(writeStream);
 };
